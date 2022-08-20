13049335d3bcda9cc75eeecb05d52a323d904df0
Anzhela Nerukh <77067921+Angiee-n@users.noreply.github.com>
2022-08-20 10:30:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+//"use strict"
+/*let isButtonclick = false;
+const buttonDiseble = document.querySelectorAll('.control-menu-button');
+for (let elem of buttonDiseble) {
+    elem.addEventListener('click', () => {
+        isButtonclick = true;
+
+    })
+}*/
